b44a43f3d140b2ecee107021c5db3be197b2afee
mousebyte <ateague063@gmail.com>
2023-01-04 12:14:12 -0500

    Fix module paths

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 return {
-    focus_indicator = require("util.widget.focus-indicator"),
-    action_box      = require("util.widget.action-box"),
-    fuzzy_select    = require("util.widget.fuzzy-select"),
-    bind            = require("util.moon.bind"),
-    screen_handler  = require("util.screen-handler"),
-    animate         = require("util.animate"),
-    fzy             = require("fzy_lua")
+    focus_indicator = require("awesome-util.widget.focus-indicator"),
+    action_box      = require("awesome-util.widget.action-box"),
+    fuzzy_select    = require("awesome-util.widget.fuzzy-select"),
+    bind            = require("awesome-util.moon.bind"),
+    screen_handler  = require("awesome-util.screen-handler"),
+    --animate         = require("util.animate"),
+    fzy             = require("awesome-util.fzy_lua")
 }
