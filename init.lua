1dfb3cf2449c66765499e1e0db89ee6bb2b3ca24
mousebyte <ateague063@gmail.com>
2023-01-04 20:26:53 -0500

    Remove screen-handler.lua

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ return {
     action_box      = require("awesome-util.widget.action-box"),
     fuzzy_select    = require("awesome-util.widget.fuzzy-select"),
     bind            = require("awesome-util.moon.bind"),
-    screen_handler  = require("awesome-util.screen-handler"),
     --animate         = require("util.animate"),
     fzy             = require("awesome-util.fzy_lua")
 }
