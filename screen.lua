ec207245ec749b7397fd7a244405132ceb723b5b
mousebyte <ateague063@gmail.com>
2023-01-06 12:32:55 -0500

    Load only awful.screen

--- screen.lua
+++ screen.lua
@@ -1,9 +1,9 @@
--- Basically wraps `awful.screen` to add a signal for focused screen changes.
+-- Basically wraps `base` to add a signal for focused screen changes.
 -- The signal is automatically emitted when a client on a different screen is focused,
 -- and when a client's screen property changes while it is focused.
 -- example: `screen.connect_signal("focused", function(s) do_stuff(s.index) end)`
 
-local awful = require("awful")
+local base = require("awful.screen")
 local gmath = require("gears.math")
 
 local capi = {
@@ -19,14 +19,14 @@ end
 
 function screen.focus(s)
     s = get_screen(s)
-    if s and s ~= awful.screen.focused() then
-        awful.screen.focus(s)
+    if s and s ~= base.focused() then
+        base.focus(s)
         capi.screen.emit_signal("_focused", s)
     end
 end
 
 function screen.focus_bydirection(dir, s)
-    s = get_screen(s or awful.screen.focused())
+    s = get_screen(s or base.focused())
     local target = s:get_next_in_direction(dir)
 
     if target then
@@ -36,10 +36,12 @@ end
 
 function screen.focus_relative(offset)
     screen.focus(gmath.cycle(capi.screen.count(),
-        awful.screen.focused().index + offset))
+        base.focused().index + offset))
 end
 
-capi.screen.connect_signal("_focused", function(s) s:emit_signal("focused") end)
+capi.screen.connect_signal("_focused", function(s)
+    s:emit_signal("focused")
+end)
 
 capi.client.connect_signal("focus", function(c)
     screen.focus(c.screen)
@@ -49,4 +51,4 @@ capi.client.connect_signal("property::screen", function(c)
     if capi.client.focus == c then screen.focus(c.screen) end
 end)
 
-return setmetatable(screen, { __index = function(_, k) return rawget(awful.screen, k) end })
+return setmetatable(screen, { __index = function(_, k) return rawget(base, k) end })
